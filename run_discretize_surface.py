70ec443a65a032a3b4071f21c8e117e09cb7eaad
bmsamudio <bmsamudio@users.noreply.github.com>
2024-09-20 04:38:13 -0700

    Update run_discretize_surface.py
    Changes made for testing purposes

--- run_discretize_surface.py
+++ run_discretize_surface.py
@@ -1,11 +1,12 @@
 vdw_alteration = 1.8
 vdw_minimum = 1.2
 vdw_maximum = 4.2
-x_coord = -19.91500473022461
-y_coord = 5.902616500854492
-z_coord = 9.151772499084473
-radius = 5.0
-output_filename = 'test.wrl'
+x_coord = 15.243419647216797
+y_coord = 8.344330787658691
+z_coord = 4.119806289672852
+radius = 8.0
+basename = '3w32-benzene-PARTIAL-DATA-'
+count = 0
 
 cmd.pseudoatom('reference',pos="[%.2f,%.2f,%.2f]"%(x_coord,y_coord,z_coord))
 cmd.alter(selection='resname ALP',expression="vdw = q - %.2f"%vdw_alteration)
@@ -15,6 +16,7 @@ cmd.set(name='surface_type',value=1)
 objects = []
 objects = cmd.get_object_list('all')
 for obj in objects:
+        count += 1
         print(f"Operating on: {obj}")
         cmd.hide('all')
         cmd.create(name='hydro_coloring',selection="(bychain resname ALP) within %.2f of reference"%radius)
@@ -24,4 +26,5 @@ for obj in objects:
         cmd.spectrum(expression='vdw',palette='rainbow',selection='vdw_coloring',minimum="%.2f"%vdw_minimum,maximum="%.2f"%vdw_maximum)
         cmd.show(representation='surface',selection='hydro_coloring')
         cmd.show(representation='surface',selection='vdw_coloring')
+        basename + str(count) + ".wrl"
         cmd.save(filename="%s"%output_filename,selection='enabled')
