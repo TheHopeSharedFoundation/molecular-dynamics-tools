f9ee5e57f86c8df0ebfdb35383b18b7ca9c2407c
bmsamudio <bmsamudio@users.noreply.github.com>
2024-09-21 05:17:45 -0700

    Update run_discretize_surface.py

--- run_discretize_surface.py
+++ run_discretize_surface.py
@@ -8,7 +8,7 @@ radius = 8.0
 basename = '3w32-benzene-PARTIAL-DATA-'
 count = 0
 
-cmd.pseudoatom('reference',pos="[%.2f,%.2f,%.2f]"%(x_coord,y_coord,z_coord))
+
 cmd.alter(selection='resname ALP',expression="vdw = q - %.2f"%vdw_alteration)
 cmd.color(color='magenta',selection='all')
 cmd.set(name='surface_quality',value=0) # For fingerprint generation which only references the geometric center, consider making this value higher
@@ -19,12 +19,16 @@ for obj in objects:
         count += 1
         print(f"Operating on: {obj}")
         cmd.hide('all')
+        cmd.pseudoatom('reference',pos="[%.2f,%.2f,%.2f]"%(x_coord,y_coord,z_coord))
         cmd.create(name='hydro_coloring',selection="(bychain resname ALP) within %.2f of reference"%radius)
         cmd.create(name='vdw_coloring',selection="(bychain resname ALP) within %.2f of reference"%radius)
+        cmd.delete('reference')
         cmd.color(color='white',selection='hydro_coloring and elem O')
         cmd.color(color='black',selection='hydro_coloring and elem C')
         cmd.spectrum(expression='vdw',palette='rainbow',selection='vdw_coloring',minimum="%.2f"%vdw_minimum,maximum="%.2f"%vdw_maximum)
         cmd.show(representation='surface',selection='hydro_coloring')
         cmd.show(representation='surface',selection='vdw_coloring')
-        basename + str(count) + ".wrl"
+        output_filename = basename + str(count) + ".wrl"
         cmd.save(filename="%s"%output_filename,selection='enabled')
+        cmd.delete('hydro_coloring')
+        cmd.delete('vdw_coloring')
