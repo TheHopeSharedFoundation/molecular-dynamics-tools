2685a49bc6644c8470b19dc9037dbe97aef53ad0
bmsamudio <bmsamudio@users.noreply.github.com>
2024-11-11 14:15:43 -0800

    Update run_histogram_3d.py

--- run_histogram_3d.py
+++ run_histogram_3d.py
@@ -33,9 +33,10 @@ current_time = now.strftime("%Y%b%dUTC%H%M%S").capitalize()
 
 parser = argparse.ArgumentParser(prog='3D-bin alpha sphere and atom positions',description='This program 3D-bins alpha sphere and atom positions',epilog='Copyright 2024 The Hope Shared Foundation')
 
-parser.add_argument('--directory',help="This is the directory that contains the alpha-sphere PQR files or atom TXT files",required=True)
+parser.add_argument('--filepath',help="This is the directory that contains the alpha-sphere PQR files or atom TXT files",required=True)
 parser.add_argument('--type',default="alpha",help="This is the type of data being binned.  alpha for alpha-spheres and atom for atom coordinates",required=True)
 parser.add_argument('--basename',default=current_time,help="This is the basename of the output file which will be created.",required=True)
+parser.add_argument('--cutoff',default=current_time,help="B-factor cutoff value",required=True)
 
 args = parser.parse_args()
 
@@ -61,86 +62,40 @@ bin_number = [x_number_bins,y_number_bins,z_number_bins]
 
 if args.type == "alpha":
     
-    path = Path(args.directory)
-    file_stem = path.stem
-    dir_name_pqr = str(file_stem) + "_pqr_vdw_files"
-    cmd_rm = "rm -r " + dir_name_pqr
-    os.system(cmd_rm)
-    cmd_mkdir = "mkdir " + dir_name_pqr
-    os.system(cmd_mkdir)
-    
-    # Initiate lists.  The "coordinates" list will contain the x, y, and z coorinates of each alpha sphere.  The "radii" list will contain the van der Waals radius of each alpha sphere.
     coordinates = []
     radii = []
-    
-    atom_type = "ATOM"
     atom_index = 0
-    atom_name = "AS"
-    alt_loc = ""
-    res_name = "ALP"
-    chain = "A"
-    res_index = 1
-    insertion_code = ""
-    occupancy = 1.00
-    b_factor = 0.00
-    element_sym = "Ne"
-    altered_pqr_count = 0
 
-    # Search through a directory and find all PQR files.  For each PQR file and each alpha sphere, extract the x, y, and z coordinates and van der Waals radius.
-    print(Path(args.directory))
-    pqr_files = str(Path(args.directory)) + '/*.pqr'
-    for name in glob.glob(pqr_files):
-        altered_pqr_count += 1
-        altered_pqr_name = str(path.stem) + "_" + str(altered_pqr_count) + ".pdb"
-        with open(altered_pqr_name,'w') as output_file:
-            print(name) 
-            with open(name) as f:
-                for file_row in f:
-                    row = re.search(r"^ATOM\s+\d+\s+(\w+)\s+\w+\s+\d+\s+([-+]?\d+\.\d*e?[-+]?\d*)\s+([-+]?\d+\.\d*e?[-+]?\d*)\s+([-+]?\d+\.\d*e?[-+]?\d*)\s+[-+]?\d+\.\d*e?[-+]?\d*\s+([-+]?\d+\.\d*e?[-+]?\d*)", file_row)
-                    if row:
-                        atom_index += 1
-                        elem=row.group(1)
-                        x=row.group(2)
-                        y=row.group(3)
-                        z=row.group(4)
-                        vdw=row.group(5)
-                        #print(elem, x, y, z, vdw)
-                        coordinates.append([float(x), float(y), float(z)])
-                        radii.append(float(vdw))
-                        occupancy = vdw
-                        output_file.write(f"{atom_type:6s}{atom_index:5d} {atom_name:^4s}{alt_loc:1s}{res_name:3s} {chain:1s}{res_index:4d}{insertion_code:1s}   {float(x):8.3f}{float(y):8.3f}{float(z):8.3f}{float(occupancy):6.2f}{b_factor:6.2f}          {elem:>2s}" + "\n")
-
-
-    print(len(coordinates))
-    print(len(radii))
+    with open(args.filepath,'r') as f:
+        for file_row in f:
+            row = re.search(r"^ATOM\s+\d+\s+(\w+)\s+\w+\s+\d+\s+([-+]?\d+\.\d*e?[-+]?\d*)\s+([-+]?\d+\.\d*e?[-+]?\d*)\s+([-+]?\d+\.\d*e?[-+]?\d*)\s+[-+]?\d+\.\d*e?[-+]?\d*\s+([-+]?\d+\.\d*e?[-+]?\d*)", file_row)
+            if row:
+                atom_index += 1
+                elem=row.group(1)
+                x=row.group(2)
+                y=row.group(3)
+                z=row.group(4)
+                vdw=row.group(5)
+                coordinates.append([float(x), float(y), float(z)])
+                radii.append(float(vdw))
 
 elif args.type == "atom":
-    
-    # Initiate lists.  The "coordinates" list will contain the x, y, and z coorinates of each alpha sphere.  The "radii" list will contain the van der Waals radius of each alpha sphere.
+
     coordinates = []
     radii = []
 
-    # Search through a directory and find all PQR files.  For each PQR file and each alpha sphere, extract the x, y, and z coordinates and van der Waals radius.
-    txt_files = str(Path(args.directory))
-    for name in glob.glob(txt_files): 
-        print(name) 
-        with open(name) as f:
-            for file_row in f:
-                row = re.search(r"([-+]?\d+\.\d*e?[-+]?\d*)\s+([-+]?\d+\.\d*e?[-+]?\d*)\s+([-+]?\d+\.\d*e?[-+]?\d*)", file_row)
-                if row:
-                    x=row.group(1)
-                    y=row.group(2)
-                    z=row.group(3)
-                    vdw=1
-                    print(float(x),float(y), float(z), float(vdw))
-                    coordinates.append([float(x), float(y), float(z)])
-                    radii.append(float(vdw))
+    with open(args.filepath,'r') as f:
+        for file_row in f:
+            row = re.search(r"([-+]?\d+\.\d*e?[-+]?\d*)\s+([-+]?\d+\.\d*e?[-+]?\d*)\s+([-+]?\d+\.\d*e?[-+]?\d*)", file_row)
+            if row:
+                x=row.group(1)
+                y=row.group(2)
+                z=row.group(3)
+                vdw=1
+                print(float(x),float(y), float(z), float(vdw))
+                coordinates.append([float(x), float(y), float(z)])
+                radii.append(float(vdw))
                     
-
-
-    print(len(coordinates))
-    print(len(radii))
-
 ################################################################## Bin alpha sphere coordinates and calculate the mean van der Waals radius for each bin
 
 # Bin alpha sphere coordinates, calculate the mean van der Waals radius for each bin, and output a single PDB file with normalized bin counts in the occupancy column and mean van der Waals in the b-factor column.
@@ -150,39 +105,51 @@ y_start = 0
 z_start = 0
 atom_type = "ATOM"
 atom_index = 0
-atom_name = "AS"
+atom_name = "Ne"
 alt_loc = ""
 res_name = "ALP"
 chain = "A"
 res_index = 1
 insertion_code = ""
-occupancy = 1.00
+occupancy = 0.00
 b_factor = 0.00
 element_sym = "Ne"
 
 coordinates_arranged = np.array(coordinates)
 coordinates_arranged.shape = (len(coordinates),3)
 
-counts = stats.binned_statistic_dd(coordinates_arranged, radii, bins=bin_number,range=bin_range, statistic = 'count')
-means = stats.binned_statistic_dd(coordinates_arranged,radii,bins=bin_number,range=bin_range,statistic ='mean',expand_binnumbers='True',binned_statistic_result=counts)
+res = stats.binned_statistic_dd(coordinates_arranged, radii, bins=bin_number,range=bin_range, statistic = 'count')
+res2 = stats.binned_statistic_dd(coordinates_arranged,radii,bins=bin_number,range=bin_range,statistic ='mean',binned_statistic_result=res)
 
-maximum = np.max(counts.statistic)
-print(maximum)
+maximum = np.max(res.statistic)
+print(f"MAXIMUM: {maximum}")
 
 output_pdb_file = args.basename + "_3dBinned.pdb"
-f = open(output_pdb_file,"w")
-count = 0
-for i in range(0,len(means.binnumber[0])):
-    count += 1
-    #print(i,means.binnumber[0][i],means.binnumber[1][i],means.binnumber[2][i])
-    x_coord = x_bin_min + (means.binnumber[0][i] * x_bin_increment)
-    y_coord = y_bin_min + (means.binnumber[1][i] * y_bin_increment) 
-    z_coord = z_bin_min + (means.binnumber[2][i] * z_bin_increment) 
-    b_factor = counts.statistic[means.binnumber[0][i]][means.binnumber[1][i]][means.binnumber[2][i]] / maximum
-    occupancy = means.statistic[means.binnumber[0][i]][means.binnumber[1][i]][means.binnumber[2][i]]
-    #count_value = counts.statistic[0][0][0]
-    #print(count,means.binnumber[0][i],means.binnumber[1][i],means.binnumber[2][i],x_coord,y_coord,z_coord,occupancy,b_factor)
-    if b_factor > 0:
-        f.write(f"{atom_type:6s}{atom_index:5d} {atom_name:^4s}{alt_loc:1s}{res_name:3s} {chain:1s}{res_index:4d}{insertion_code:1s}   {x_coord:8.3f}{y_coord:8.3f}{z_coord:8.3f}{occupancy:6.2f}{b_factor:6.2f}          {element_sym:>2s}" + "\n")
 
-f.close()
+count = 0
+
+x_old = 0
+y_old = 0
+z_old = 0
+x_new = 0
+y_new = 0
+z_new = 0
+
+pdb_output = []
+
+with open(output_pdb_file,'w') as pdb_output_handle:
+    for x in range(0,x_number_bins):
+        x_coord = x_bin_min + (x * x_bin_increment)
+        for y in range(0,y_number_bins):
+            y_coord = y_bin_min + (y * y_bin_increment)
+            for z in range(0,z_number_bins):
+                z_coord = z_bin_min + (z * z_bin_increment)
+                b_factor = res.statistic[x][y][z] / maximum
+                occupancy = res2.statistic[x][y][z]
+                if b_factor >= float(args.cutoff):
+                    print(b_factor,occupancy)
+                    atom_index += 1
+                    atom_code = format(atom_index,'#07x')
+                    pdb_output_handle.write(f"{atom_type:6s}{atom_code[2:]:5s} {atom_name:^4s}{alt_loc:1s}{res_name:3s} {chain:1s}{res_index:4d}{insertion_code:1s}   {x_coord:8.3f}{y_coord:8.3f}{z_coord:8.3f}{occupancy:6.2f}{b_factor:6.2f}          {element_sym:>2s}" + "\n")
+
+
