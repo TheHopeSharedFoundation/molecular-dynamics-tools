0bc361bda93ea9f54d3c9b2a6c9ae028457f6ad0
bmsamudio <bmsamudio@users.noreply.github.com>
2024-11-11 16:30:19 -0800

    Update show_occupancy_isosurfaces_pymol_commands.py

--- show_occupancy_isosurfaces_pymol_commands.py
+++ show_occupancy_isosurfaces_pymol_commands.py
@@ -18,15 +18,19 @@
 # Towards Alleviating Suffering                                            #
 ############################################################################
 
-cmd.create("alpha_0.9to1.0",selection="(resname ALP and b >0.9) and (resname ALP and (b < 1.0 or b=1.0))")
-cmd.create("alpha_0.8to0.9",selection="(resname ALP and b >0.8) and (resname ALP and (b < 0.9 or b=0.9))")
-cmd.create("alpha_0.7to0.8",selection="(resname ALP and b >0.7) and (resname ALP and (b < 0.8 or b=0.8))")
-cmd.create("alpha_0.6to0.7",selection="(resname ALP and b >0.6) and (resname ALP and (b < 0.7 or b=0.7))")
-cmd.create("alpha_0.5to0.6",selection="(resname ALP and b >0.5) and (resname ALP and (b < 0.6 or b=0.6))")
-cmd.create("alpha_0.4to0.5",selection="(resname ALP and b >0.4) and (resname ALP and (b < 0.5 or b=0.5))")
-cmd.create("alpha_0.3to0.4",selection="(resname ALP and b >0.3) and (resname ALP and (b < 0.4 or b=0.4))")
-cmd.create("alpha_0.2to0.3",selection="(resname ALP and b >0.2) and (resname ALP and (b < 0.3 or b=0.3))")
-cmd.create("alpha_0.1to0.2",selection="(resname ALP and b >0.1) and (resname ALP and (b < 0.2 or b=0.2))")
-cmd.create("alpha_0.0to0.1",selection="(resname ALP and b >0.0) and (resname ALP and (b < 0.1 or b=0.1))")
-cmd.group("alpha",members="alpha*")
-cmd.show(representation="mesh",selection="alpha")
+obj_count = 0
+obj_list =  cmd.get_object_list(selection='*3dBinned')
+for obj in obj_list:
+	obj_count += 1
+	cmd.create("%s_alpha_0.9to1.0"%obj_count,selection="(%s and resname ALP and b >0.9) and (%s and resname ALP and (b < 1.0 or b=1.0))"%(obj,obj))
+	cmd.create("%s_alpha_0.8to0.9"%obj_count,selection="(%s and resname ALP and b >0.8) and (%s and resname ALP and (b < 0.9 or b=0.9))"%(obj,obj))
+	cmd.create("%s_alpha_0.7to0.8"%obj_count,selection="(%s and resname ALP and b >0.7) and (%s and resname ALP and (b < 0.8 or b=0.8))"%(obj,obj))
+	cmd.create("%s_alpha_0.6to0.7"%obj_count,selection="(%s and resname ALP and b >0.6) and (%s and resname ALP and (b < 0.7 or b=0.7))"%(obj,obj))
+	cmd.create("%s_alpha_0.5to0.6"%obj_count,selection="(%s and resname ALP and b >0.5) and (%s and resname ALP and (b < 0.6 or b=0.6))"%(obj,obj))
+	cmd.create("%s_alpha_0.4to0.5"%obj_count,selection="(%s and resname ALP and b >0.4) and (%s and resname ALP and (b < 0.5 or b=0.5))"%(obj,obj))
+	cmd.create("%s_alpha_0.3to0.4"%obj_count,selection="(%s and resname ALP and b >0.3) and (%s and resname ALP and (b < 0.4 or b=0.4))"%(obj,obj))
+	cmd.create("%s_alpha_0.2to0.3"%obj_count,selection="(%s and resname ALP and b >0.2) and (%s and resname ALP and (b < 0.3 or b=0.3))"%(obj,obj))
+	cmd.create("%s_alpha_0.1to0.2"%obj_count,selection="(%s and resname ALP and b >0.1) and (%s and resname ALP and (b < 0.2 or b=0.2))"%(obj,obj))
+	cmd.create("%s_alpha_0.0to0.1"%obj_count,selection="(%s and resname ALP and b >0.0) and (%s and resname ALP and (b < 0.1 or b=0.1))"%(obj,obj))
+	cmd.group("%s_alpha"%obj_count,members="%s_alpha*"%obj_count)
+	cmd.show(representation="mesh",selection="%s_alpha"%obj_count)
