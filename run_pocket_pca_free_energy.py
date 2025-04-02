e55d96f37b41dd1af5685de1cf524be0e63fcfe2
bmsamudio <bmsamudio@users.noreply.github.com>
2025-04-01 21:25:15 -0700

    Update run_pocket_pca_free_energy.py
    Made this script simpler

--- run_pocket_pca_free_energy.py
+++ run_pocket_pca_free_energy.py
@@ -14,6 +14,7 @@ from scipy.sparse import csr_matrix
 from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.cluster import KMeans
 import argparse
 import pandas as pd
@@ -22,115 +23,20 @@ import plotly.express as px
 import random
 from collections import Counter
 
-
-def jaccard_similarity(doc1, doc2):
-	words_doc1 = set(doc1.split())
-	words_doc2 = set(doc2.split())
-	intersection = words_doc1.intersection(words_doc2)
-	union = words_doc1.union(words_doc2)
-	return len(intersection) / len(union)
-
-df = pd.read_csv("/Users/benjaminsamudio/grid_based_fingerprints.csv")
-
-####################################  Testing start
-
-# The following is based on: https://pmc.ncbi.nlm.nih.gov/articles/PMC2538559/
-
-number_references = 10
-number_subsamples = 10
-picoseconds_per_frame = 10
-fractional_occupancy = 1 / number_references
-number_frames = len(df)
-block_size = int(number_frames * fractional_occupancy)
-
-#df =df.head(block_size)
-
-
-# Initialize the bin array
-
-df['jaccard_similarity'] = 0
-df['structure_bin_index'] = -1
-
-for bin_index in range(0,number_references):
-	# Filter DataFrame
-	filtered_df = df[df['structure_bin_index'] == -1]
-
-	random_index = random.randint(0, len(filtered_df))
-	print(f"Random row: {random_index} bin_index: {bin_index}\n")
-
-	for index in range(0,len(filtered_df)):
-		similarity = jaccard_similarity(filtered_df['fingerprint'].iloc[random_index],filtered_df['fingerprint'].iloc[index])
-		filtered_df['jaccard_similarity'].iloc[index] = similarity
-		filtered_df['structure_bin_index'].iloc[index] = bin_index
-
-	filtered_df.sort_values(by='jaccard_similarity',inplace=True,ascending=False)
-	filtered_df = filtered_df.head(block_size)
-	print(filtered_df)
-	df.update(filtered_df)
-	
-print(df)
-
-observed_list = []
-frame_list = []
-
-for i in range(0,block_size):
-	output_bin_index = 0
-	output_string = ""
-	counter_list = []
-	for j in range(0,number_references):
-		bin_identifier = df['structure_bin_index'].iloc[output_bin_index]
-		counter_list.append(int(bin_identifier))
-		output_bin_index = output_bin_index + i
-		output_string = output_string + "," + str(bin_identifier)
-	#print(i,output_string)
-	bin_frequencies = Counter(counter_list)
-	variance = 0
-	observed = 0
-	for key, value in bin_frequencies.items():
-		variance = variance + ((value/number_references - 1/number_references)**2)/number_references
-		observed = variance / ( (1/number_references*(1-1/number_references))/number_references * ((len(df)-number_references)/(len(df)-1))    )
-	frame_list.append(i)
-	observed_list.append(observed)	
-	print(variance,observed)
-# trendline="rolling", trendline_options=dict(window=5)
-
-fig_4 = px.scatter(x=frame_list,y=observed_list,log_y=True,log_x=True)
-fig_4.update_traces(mode='lines')
-fig_4.show()
-fig_4.write_html("benzene_pocket_convergence_analysis.html")
-
-#################################    Testing end
-
-
-'''
-
-
-
-#print(df['fingerprint'].head())
+df = pd.read_csv("/Users/benjaminsamudio/grid_based_fingerprints-TEST.csv")
 fingerprint_list = []
 fingerprint_list = df['fingerprint'].astype(str)
 
-#tfidf_vectorizer = TfidfVectorizer(max_features=5000)
 tfidf_vectorizer = TfidfVectorizer()
 X_tfidf = tfidf_vectorizer.fit_transform(fingerprint_list)
+array_tfidf = X_tfidf.toarray()
 
-print(X_tfidf)
-
-jaccard_sim = jaccard_similarity(df['fingerprint'].iloc[0], df['fingerprint'].iloc[1])
-print(f"Jaccard Similarity: {jaccard_sim}")
-
-#cosine_sim = cosine_similarity(X_tfidf[0:1], X_tfidf)
-#print(f"Cosine Similarity: {cosine_sim[0][1]}")
-
-#text_features = CountVectorizer(analyzer="word").fit_transform(fingerprint_list)
-#print(text_features)
+#cosine_sim = cosine_similarity(array_tfidf, array_tfidf)
 
 clusterer = KMeans(n_clusters=5)
 clusters = clusterer.fit_predict(X_tfidf)
-X_pca = PCA(n_components=2).fit_transform(X_tfidf.toarray())
 
-#pca_x_list = []
-#pca_y_list = []
+X_pca = PCA(n_components=2).fit_transform(X_tfidf.toarray())
 
 
 with open("pca_analysis_of_pockets.csv","w") as pca_out:
@@ -220,6 +126,3 @@ fig_2 = px.density_contour(df_3, x="xcenter", y="ycenter", z="pmf", histfunc="mi
 fig_2.update_traces(contours_coloring="heatmap", contours_showlabels = False, colorscale="rainbow")
 fig_2.show()
 fig_2.write_html("benzene_pocket_analysis_free_energy_landscape.html")
-
-
-'''
